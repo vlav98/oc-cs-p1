3d26debe42c6eae9a3e6c2d2b4da4007b05943fe
Valentine <valentinelav98@gmail.com>
2020-08-03 14:46:20 +0200

    :sparkles: Initial project

---
+++ MaPremiereApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaPremiereApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
