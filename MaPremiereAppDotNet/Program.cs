f0d28ef0ac56b6ae183d525189bbb33d1d993bc8
Valentine <valentinelav98@gmail.com>
2020-08-03 16:53:16 +0200

    Correct new project added

---
+++ MaPremiereAppDotNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaPremiereAppDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = Complex.One;
+            Console.WriteLine(c);
+            Console.WriteLine("Partie réelle : " + c.Real);
+            Console.WriteLine("Partie imaginaire : " + c.Imaginary);
+
+            Console.WriteLine(Complex.Conjugate(Complex.FromPolarCoordinates(1.0, 45 * Math.PI / 180)));
+        }
+    }
+}
