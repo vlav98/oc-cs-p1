f63fd7c971cf5cd86aa79d3290047972371e7e1b
Valentine <valentinelav98@gmail.com>
2020-08-03 16:53:41 +0200

    :beers: -

--- MaPremiereApplication/Program.cs
+++ MaPremiereApplication/Program.cs
@@ -1,36 +1,10 @@
-﻿using System;
-
-namespace MaPremiereApplication
+﻿namespace MaPremiereApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WelcomeMessage("Valentine", 22);
-            double valeur = LongueurHypotenuse(1, 3);
-            Console.WriteLine(valeur);
-            valeur = LongueurHypotenuse(10, 10);
-            Console.WriteLine("Le résultat est : " + valeur);
-        }
-
-        static void AffichageBienvenue()
-        {
-            Console.WriteLine("Bonjour Valentine");
-            Console.WriteLine("-------" + Environment.NewLine);
-            Console.WriteLine("\tBienvenue dans le monde merveilleux du C#");
-        }
-
-        static void WelcomeMessage(string name, int age)
-        {
-            Console.WriteLine("Bonjour " + name);
-            Console.WriteLine("Vous avez " + age + " ans");
-        }
-
-        static double LongueurHypotenuse(double a, double b)
-        {
-            double sommeDesCarres = a * a + b * b;
-            double resultat = Math.Sqrt(sommeDesCarres);
-            return resultat;
+            System.Xml.ConformanceLevel level;
         }
     }
 }
