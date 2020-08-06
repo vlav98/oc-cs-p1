470e0efcee04804d742b1b8dc296591ca731617b
Valentine <valentinelav98@gmail.com>
2020-08-06 10:20:31 +0200

    :truck: New project for TP1

---
+++ TP1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaPremiereAppDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            message();
+        }
+
+        static void message ()
+        {
+            DayOfWeek day = DateTime.Now.DayOfWeek;
+            int hour = DateTime.Now.Hour;
+
+            if (day == DayOfWeek.Saturday || day == DayOfWeek.Sunday)
+            {
+                BonWeekEnd();
+            } else
+            {
+                if (hour >= 9 && hour < 18)
+                {
+                    Bonjour();
+                } else
+                {
+                    if (day == DayOfWeek.Friday)
+                    {
+                        BonWeekEnd();
+                    } else
+                    {
+                        Bonsoir();
+                    }
+                }
+            }
+        }
+
+        static void BonWeekEnd()
+        {
+            Console.WriteLine("Bon week-end " + Environment.UserName);
+        }
+        static void Bonjour()
+        {
+            Console.WriteLine("Bonjour " + Environment.UserName);
+        }
+        static void Bonsoir()
+        {
+            Console.WriteLine("Bonsoir " + Environment.UserName);
+        }
+    }
+}
